76f1fd34b94297178ba3344a2183cef1812dd8fa
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-04-30 20:56:03 +0300

    add lunar

--- main32.sh
+++ main32.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 set -e
 # Add dependent repositories
+sudo dpkg --add-architecture i386
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
