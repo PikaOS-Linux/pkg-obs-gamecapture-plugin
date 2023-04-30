1bc1a02117eca3fb9573b1990dc0de0b4d8483c5
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-04-30 20:37:23 +0300

    add lunar

---
+++ main.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -e
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+git clone https://github.com/nowrep/obs-vkcapture
+cp -rvf ./debian ./obs-vkcapture/
+cd ./obs-vkcapture
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage --no-sign
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
